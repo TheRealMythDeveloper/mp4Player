695e3a280efd6ed7ee7932f0a40c22e8e7b8279e
TheRealMythDeveloper <therealmythdev@gmail.com>
2026-06-17 15:22:29 -0600

    Add files via upload

---
+++ mp4player.js.js
@@ -0,0 +1,7 @@
+export const moduleData = {
+  id: "mp4player",
+  icon: "🎬",
+  title: "Video Player",
+  desc: "Play local MP4 videos like Spotify.",
+  entry: "js/modules/mp4player/index.html"
+};
