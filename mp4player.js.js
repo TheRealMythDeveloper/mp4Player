633e95c138930a0866e28b25e03ce084e09cb2f9
TheRealMythDeveloper <therealmythdev@gmail.com>
2026-06-17 15:24:07 -0600

    Delete mp4player.js.js

--- mp4player.js.js
+++
@@ -1,7 +0,0 @@
-export const moduleData = {
-  id: "mp4player",
-  icon: "🎬",
-  title: "Video Player",
-  desc: "Play local MP4 videos like Spotify.",
-  entry: "js/modules/mp4player/index.html"
-};
